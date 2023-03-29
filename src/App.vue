c709a76d1457c8b31e8f03a857188f21e678c026
emanuelevolpes <emanuele.volpes1@gmail.com>
2023-03-29 16:14:51 +0200

    searchFilms solved

--- src/App.vue
+++ src/App.vue
@@ -20,14 +20,12 @@ export default {
             console.log(this.store.searchKey)
             //chiamata api
             axios
-            .get('https://api.themoviedb.org/3/search/movie?api_key=e99307154c6dfb0b4750f6603256716d&query=')
+            .get(`https://api.themoviedb.org/3/search/movie?api_key=e99307154c6dfb0b4750f6603256716d&query=${store.searchKey}`)
             .then((response) => {
                 console.log(response)
             })
 
         }
-    },mounted() {
-        this.searchFilms()
     }
 }
 </script>
