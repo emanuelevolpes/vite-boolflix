3e72cf7adb7d4e3b11b7b2b87dd78fac6bbf2c88
emanuelevolpes <emanuele.volpes1@gmail.com>
2023-03-29 16:53:26 +0200

    v-for films.original_name doesn't work

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -15,4 +15,9 @@ export default {
         <input type="text" name="searchBar" id="searchBar" v-model="store.searchKey">
         <button type="button" @click.prevent="$emit('search')">Cerca</button>
     </div>
+    <div v-for="films in store.resultSearchFilm.results">
+        <ul v-for="film in films.original_name">
+            <li>{{ film }}</li>
+        </ul>
+    </div>
 </template>
