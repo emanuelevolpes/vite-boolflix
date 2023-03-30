10eed43349e758a93a81c1925d237c04a9d4a03c
emanuelevolpes <emanuele.volpes1@gmail.com>
2023-03-30 16:13:45 +0200

    getLanguage case

--- src/components/CardApp.vue
+++ src/components/CardApp.vue
@@ -7,6 +7,30 @@ export default {
     },
     components: {
         CountryFlag
+    },
+    computed: {
+        getLanguage() {
+            switch (this.info.original_language) {
+                case 'en':
+                    return 'gb';
+                case 'he':
+                    return 'il';
+                case 'hi':
+                    return 'il';
+                case 'ja':
+                    return 'jp';
+                case 'zh':
+                    return 'cn';
+                case 'fa':
+                    return 'ir';
+                case 'ko':
+                    return 'gn';
+                case 'cs':
+                    return 'cz';
+                default:
+                    return this.info.original_language;
+            }
+        }
     }
 }
 </script>
@@ -14,6 +38,6 @@ export default {
 <template>
     <li>Titolo: {{ info.title }}</li>
     <li>Titolo originale: {{ info.original_title }}</li>
-    <li>Lingua originale: {{ info.original_language }}</li>
+    <li>Lingua originale: {{ getLanguage }} <country-flag :country='getLanguage' size='small' /></li>
     <li>Voto: {{ info.vote_average }}</li>
 </template>
