f5c3fecb4b0926f483fb02b7362629c039b97765
emanuelevolpes <emanuele.volpes1@gmail.com>
2023-03-31 14:56:31 +0200

    method search with store config

--- src/store.js
+++ src/store.js
@@ -1,7 +1,13 @@
 import { reactive } from 'vue';
 
-export const store = reactive ({
+export const store = reactive({
+    config: {
+        urlApi: 'https://api.themoviedb.org/3',
+        searchMovie: '/search/movie',
+        searchSeries: '/search/tv',
+        apiKey: 'e99307154c6dfb0b4750f6603256716d'
+    },
     searchKey: '',
-    resultSearchFilm : [],
-    resultSearchSeries : []
+    resultSearchFilm: [],
+    resultSearchSeries: []
 })
