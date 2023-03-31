fa0e40b8df9aceedbab891d276eb941a09933bbe
emanuelevolpes <emanuele.volpes1@gmail.com>
2023-03-31 15:07:22 +0200

    only one card

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,13 +1,11 @@
 <script>
 import { store } from '../store'
-import CardFilms from './CardFilms.vue';
-import CardSeries from './CardSeries.vue'
+import Card from './Card.vue'
 export default {
     name: 'Main',
     components: {
-    CardFilms,
-    CardSeries
-},
+        Card
+    },
     data() {
         return {
             store
@@ -17,14 +15,16 @@ export default {
 </script>
 
 <template>
-    <div>
-        <ul v-for="films in store.resultSearchFilm">
-            <CardFilms :infoFilm="films" />
-        </ul>
-    </div>
-    <div>
-        <ul v-for="series in store.resultSearchSeries">
-            <CardSeries :infoSeries="series" />
-        </ul>
+    <div class="d-flex">
+        <div>
+            <ul v-for="films in store.resultSearchFilm">
+                <Card :info="films" />
+            </ul>
+        </div>
+        <div>
+            <ul v-for="series in store.resultSearchSeries">
+                <Card :info="series" />
+            </ul>
+        </div>
     </div>
 </template>
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,17 +1,16 @@
 <script>
 import CountryFlag from 'vue-country-flag-next'
-
 export default {
-    name: 'CardFilms',
+    name: 'Card',
     props: {
-        infoFilm: Object
+        info: Object
     },
     components: {
         CountryFlag
     },
     computed: {
         getLanguage() {
-            switch (this.infoFilm.original_language) {
+            switch (this.info.original_language) {
                 case 'en':
                     return 'gb';
                 case 'he':
@@ -29,11 +28,11 @@ export default {
                 case 'cs':
                     return 'cz';
                 default:
-                    return this.infoFilm.original_language;
+                    return this.info.original_language;
             }
         },
         getVote() {
-            return Math.ceil(this.infoFilm.vote_average / 2);
+            return Math.ceil(this.info.vote_average / 2);
         }
     }
 }
@@ -41,9 +40,19 @@ export default {
 
 <template>
     <div>
-        <li><img :src="`https://image.tmdb.org/t/p/w300${infoFilm.backdrop_path}`" alt="copertina"></li>
-        <li>Titolo: {{ infoFilm.title }}</li>
-        <li>Titolo originale: {{ infoFilm.original_title }}</li>
+        <li><img :src="`https://image.tmdb.org/t/p/w300${info.backdrop_path}`" alt="copertina"></li>
+        <li>Titolo: {{ info.title }}</li>
+        <li>Titolo originale: {{ info.original_title }}</li>
+        <li>Lingua originale: {{ getLanguage }} <country-flag :country='getLanguage' size='small' /></li>
+        <li>Voto: {{ getVote }}
+            <font-awesome-icon icon="fa-solid fa-star" v-for="n in getVote" />
+            <font-awesome-icon icon="fa-regular fa-star" v-for="n in 5 - getVote" />
+        </li>
+    </div>
+    <div>
+        <li><img :src="`https://image.tmdb.org/t/p/w300${info.backdrop_path}`" alt="copertina"></li>
+        <li>Titolo: {{ info.name }}</li>
+        <li>Titolo originale: {{ info.original_name }}</li>
         <li>Lingua originale: {{ getLanguage }} <country-flag :country='getLanguage' size='small' /></li>
         <li>Voto: {{ getVote }}
             <font-awesome-icon icon="fa-solid fa-star" v-for="n in getVote" />
